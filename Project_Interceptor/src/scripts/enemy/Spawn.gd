f3aa38468430bdcb7f204fc381469bea7314ced9
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2022-11-19 18:42:53 +0100

    added instance life timer / changed position for enemy spawn

--- Project_Interceptor/src/scripts/enemy/Spawn.gd
+++ Project_Interceptor/src/scripts/enemy/Spawn.gd
@@ -1,9 +1,17 @@
 extends Node
 
 var enemy = preload ("res://src/nodes/Enemy_spaceship.tscn")
-var position = Vector2(-50.0,48.0)
+var position = Vector2(-50.0,30.0)
+var spawn_time = 15.0
 
 func _on_EnemyTimer_timeout():
 	var e = enemy.instance()
 	e.position = position
 	add_child(e)
+	delete_self(e)
+	
+func delete_self(e):
+	#delete after sometime for stability
+	yield(get_tree().create_timer(spawn_time), "timeout")
+	if is_instance_valid(e):
+		e.queue_free()
